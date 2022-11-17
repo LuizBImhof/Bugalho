cc21ff7396fff9a0581059ffebe49dcce5245e51
Luiz Artur Boing Imhof <77170091+LuizBImhof@users.noreply.github.com>
2022-11-17 18:28:00 -0300

    removing no longer needed tests

--- TestBugalho/UnitTest1.cs
+++ TestBugalho/UnitTest1.cs
@@ -6,41 +6,6 @@ namespace TestBugalho
     public class UnitTest1
     {
         [TestMethod]
-        public void TestBoards()
-        {
-            Game game = new();
-
-            Assert.IsNotNull(game);
-            Assert.AreEqual(3, game.boardA.columnA.Capacity);
-            Assert.AreEqual(3, game.boardA.columnB.Capacity);
-            Assert.AreEqual(3, game.boardA.columnC.Capacity);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]  
-        public void TestPlayValueOnBoard()
-        {
-            Game game = new();
-            game.PlayValue(1, 2, 0);
-            game.PlayValue(1, 4, 0);
-            game.PlayValue(1, 5, 0);
-            
-            Assert.AreEqual(2, game.boardA.columnA[0]);
-            Assert.AreEqual(4, game.boardA.columnA[1]);
-            Assert.AreEqual(5, game.boardA.columnA[2]);
-
-            game.PlayValue(1, 2, 1);
-            game.PlayValue(1, 4, 1);
-            Assert.AreEqual(2, game.boardA.columnB[0]);
-            Assert.AreEqual(4, game.boardA.columnB[1]);
-
-            game.PlayValue(3, 5, 2);
-            Assert.AreEqual(5, game.boardA.columnC[0]);
-            Assert.AreEqual(0, game.boardA.columnC[1]);
-            Assert.AreEqual(0, game.boardA.columnC[2]);
-        }
-
-        [TestMethod]
         public void TestBoardSimplePoints()
         {
             Game game = new();
